69231fa72e9e99f343227bfda5370044909f0bc5
Barbaraa7897 <58823508+Barbaraa7897@users.noreply.github.com>
2021-04-01 21:06:26 +0200

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,15 +8,6 @@ let dohvati = (req, res) => res.send(lista)
 app.get('/dohvati', dohvati) 
 
 app.listen(port, () => console.log(`Slušam na portu ${port}!`))
-////Datum
-//
-//let vrijeme=moment().format("DD.MM.YYYY, HH:mm");
-//console.log(vrijeme);
-//
-//
-//let prognoza = ['sunčano', 'kišovito', 'oblačno'];
-//let pomocni = prognoza[Math.floor(Math.random()*prognoza.length)];
-//console.log('Danas će biti ' + pomocni);
 
 var lista = [];
 while(lista.length < 10){
